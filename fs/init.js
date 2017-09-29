4d02d2899f74ed08e654cbdbd1f05e48fb6d3ba3
Philip Lundrigan <philiplundrigan@gmail.com>
2017-09-29 12:35:05 -0600

    Add more features

--- fs/init.js
+++ fs/init.js
@@ -1,11 +1,29 @@
 load('api_config.js');
 load('api_dht.js');
+load('api_mqtt.js');
 load('api_timer.js');
 
 let pin = Cfg.get('app.pin');
-let dht = DHT.create(pin, DHT.DHT22);
-let sampleTime = Cfg.get('app.sample_time')
+let sampleTime = Cfg.get('app.sample_time');
+let id = Cfg.get('device.id');
+let dataTopic = 'devices/' + id + '/data';
+let metaTopic = 'devices/' + id + '/meta';
+let sequence = 0;
 
+let dht = DHT.create(pin, DHT.DHT22);
+
+
+// Send meta data
+Timer.set(10000 /* milliseconds */, false /* repeat */, function() {
+  let message = JSON.stringify({temp: '°C', hum: '%', time: 's', seq: 'num'});
+  let ok = MQTT.pub(metaTopic, message, 1, 1);
+
+  print(message);
+  print('Published:', ok ? 'yes' : 'no');
+}, null);
+
+
+// Transmit data
 Timer.set(sampleTime /* milliseconds */, true /* repeat */, function() {
   let t = dht.getTemp();
   let h = dht.getHumidity();
@@ -15,6 +33,11 @@ Timer.set(sampleTime /* milliseconds */, true /* repeat */, function() {
     return;
   }
 
-  print('Temperature:', t, '*C');
-  print('Humidity:', h, '%');
+  let message = JSON.stringify({temp: t, hum: h, time: Timer.now(), seq: sequence});
+  let ok = MQTT.pub(dataTopic, message, 1, 0);
+
+  print(message);
+  print('Published:', ok ? 'yes' : 'no');
+
+  sequence += 1;
 }, null);
