2728b4a38751e9f62897208bf1336f0b20c05c16
Philip Lundrigan <philiplundrigan@gmail.com>
2017-11-14 10:20:36 -0700

    Implement queue_put

--- src/main.c
+++ src/main.c
@@ -14,6 +14,55 @@ enum mgos_app_init_result mgos_app_init(void) {
     return MGOS_APP_INIT_SUCCESS;
 }
 
+char *cur_file = NULL;
+
+void find_cur_file() {
+    cur_file = "data.0";
+}
+
+bool queue_put(char *data) {
+    if (cur_file == NULL) {
+        find_cur_file();
+    }
+
+    fp = fopen(cur_file, "ab");
+
+    if (fp == NULL) {
+        LOG(LL_ERROR, ("Can not open file %s", cur_file));
+        return false;
+    }
+
+    struct mg_str str_data = mg_mk_str(data);
+    LOG(LL_INFO, ("Current file: %s", cur_file));
+    LOG(LL_INFO, ("Writing data %s", str_data));
+
+    // Write length of data
+    if (fwrite(&str_data.len, sizeof(size_t), 1, fp) != sizeof(size_t)) {
+        LOG(LL_ERROR, ("Failed to write to file: %s", filename));
+        return false;
+    }
+
+    // Write actual data
+    if (fwrite(str_data.p, 1, str_data.len, fp) != str_data.len) {
+        LOG(LL_ERROR, ("Failed to write to file: %s", filename));
+        return false;
+    }
+
+    fclose(fp);
+    return true;
+}
+
+bool queue_peek() {
+    return true;
+}
+
+bool queue_delete() {
+    return true;
+}
+
+bool queue_flush() {
+    return true;
+}
 
 void write_measurement(char *data) {
     char *filename = "test";
