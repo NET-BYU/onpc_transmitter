c66764388fa5ffeb0c282b7347837f2e6595e7c9
Philip Lundrigan <philiplundrigan@gmail.com>
2018-02-05 12:57:09 -0700

    Got packet injection working!

--- src/main.c
+++ src/main.c
@@ -12,7 +12,23 @@
 #endif
 
 #include "common/cs_dbg.h"
+#include "mgos_timers.h"
 #include "mgos_app.h"
+
+
+uint8_t packet[128] = { 0x80, 0x00, 0x00, 0x00,
+                /*4*/   0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+                /*10*/  0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
+                /*16*/  0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
+                /*22*/  0xc0, 0x6c,
+                /*24*/  0x83, 0x51, 0xf7, 0x8f, 0x0f, 0x00, 0x00, 0x00,
+                /*32*/  0x64, 0x00,
+                /*34*/  0x01, 0x04,
+                /* SSID */
+                /*36*/  0x00, 0x06, 0x72, 0x72, 0x72, 0x72, 0x72, 0x72,
+                        0x01, 0x08, 0x82, 0x84,
+                        0x8b, 0x96, 0x24, 0x30, 0x48, 0x6c, 0x03, 0x01,
+                /*56*/  0x0b};
 
 
 static void promisc_cb(uint8_t *buf, uint16_t len) {
@@ -20,12 +36,45 @@ static void promisc_cb(uint8_t *buf, uint16_t len) {
   (void) buf;
 }
 
-enum mgos_app_init_result mgos_app_init(void) {
+void promiscuous_mode(void) {
     wifi_set_opmode(STATION_MODE);
-    wifi_set_channel(8);
+    wifi_set_channel(11);
     wifi_promiscuous_enable(0);
     wifi_set_promiscuous_rx_cb(promisc_cb);
-    wifi_set_channel(8);
+    wifi_set_channel(11);
     wifi_promiscuous_enable(1);
+}
+
+
+static void inject_timer_cb(void *arg) {
+    // printf("Inject!\n");
+    // packet[56] = 11;
+    wifi_send_pkt_freedom(packet, 57, 0);
+
+    (void) arg;
+}
+
+static void setup_inject_mode_timer_cb(void *arg) {
+    printf("Entering inject mode!\n");
+    wifi_set_opmode(STATION_MODE);
+    wifi_promiscuous_enable(1);
+    wifi_set_channel(11);
+
+
+    mgos_set_timer(50, MGOS_TIMER_REPEAT, inject_timer_cb, NULL);
+    printf("Done inject mode!\n");
+    (void) arg;
+}
+
+void start_inject_mode(void) {
+    printf("Starting inject mode!\n");
+    mgos_set_timer(2000, 0, setup_inject_mode_timer_cb, NULL);
+}
+
+enum mgos_app_init_result mgos_app_init(void) {
+    // promiscuous_mode();
+    printf("Starting!\n");
+    start_inject_mode();
+    printf("Done!\n");
     return MGOS_APP_INIT_SUCCESS;
 }
