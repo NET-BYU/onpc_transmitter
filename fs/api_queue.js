e6d515e2e005a05d75dd676102b76c6408c98e54
Philip Lundrigan <philiplundrigan@gmail.com>
2018-02-05 11:09:09 -0700

    Remove extra code from queue

--- fs/api_queue.js
+++
@@ -1,6 +0,0 @@
-let Queue = {
-    start: ffi('bool start_queue()'),
-    put: ffi('bool queue_put(char *)'),
-    peek: ffi('void queue_peek(void(*)(char *, userdata), userdata)'),
-    remove: ffi('bool queue_delete()'),
-};
--- fs/init.js
+++ fs/init.js
@@ -4,11 +4,9 @@ load('api_mqtt.js');
 load('api_net.js');
 load('api_sys.js');
 load('api_timer.js');
-load('api_queue.js');
 
 let led = Cfg.get('pins.led');
 let getRssi = ffi('int wifi_station_get_rssi(void)');
-let topic = '/device/x/data';
 
 let sequence = 0;
 
@@ -25,79 +23,6 @@ GPIO.set_mode(led, GPIO.MODE_OUTPUT);
 Timer.set(1000 /* 1 sec */, true /* repeat */, function() {
   let value = GPIO.toggle(led);
   print(value ? 'Tick' : 'Tock', 'uptime:', Sys.uptime(), getInfo(), getRssi());
-}, null);
-
-let minDelay = 500;
-let maxDelay = 5000;
-
-function upload_data(delay) {
-  Timer.set(delay, false, function() {
-    print("%%%%%%%%%%%%%%%%%%%%%%%%%% Peeking at data");
-    Queue.peek(function(data) {
-      print("Data peeked:", data);
-
-      if (!data) {
-        print("%%%%%%%%%%%%%%%%%%%%%%%%%% No data left, trying again later");
-        upload_data(maxDelay);
-        return;
-      }
-
-      // Valid data -- let's publish it
-      print("%%%%%%%%%%%%%%%%%%%%%%%%%% Trying to upload data");
-      let ok = MQTT.pub(topic, data, 1);
-
-      if (ok) {
-        print("%%%%%%%%%%%%%%%%%%%%%%%%%% Deleting data");
-        Queue.remove();
-        // Try getting more data
-        upload_data(minDelay);
-      }
-      else {
-        // Try again later
-        print("%%%%%%%%%%%%%%%%%%%%%%%%%% Unable to upload data -- try again later");
-        upload_data(maxDelay);
-      }
-      print("%%%%%%%%%%%%%%%%%%%%%%%%%% Done");
-    }, null);
-  }, null);
-}
-
-
-Timer.set(2000 /* 2 sec */, false /* repeat */, function() {
-  print("######################### Starting queue");
-  let startResult = Queue.start();
-  print("Result: ", startResult);
-
-  if (!startResult) {
-    print("Unable to start queue!");
-    return;
-  }
-
-  // "Generate" data
-  Timer.set(2000 /* 2 sec */, true /* repeat */, function() {
-    print("######################### Publishing data");
-    let data = JSON.stringify({
-      pm25: 100,
-      pm10: 50,
-      rssi: getRssi(),
-      sequence: sequence,
-      time: Timer.now()
-    });
-    print(data);
-    let ok = MQTT.pub(topic, data, 1);
-
-    if (!ok) {
-      print("######################### Unable to publish data");
-      print("######################### Adding data to queue");
-      let result = Queue.put(data);
-      print("Put result:", result);
-    }
-
-    sequence += 1;
-  }, null);
-
-  print("######################### Starting process to upload data");
-  upload_data(maxDelay);
 }, null);
 
 
