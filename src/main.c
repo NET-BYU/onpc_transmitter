a32c67274fb1ae4e8a1174e3b874dee624ac4201
Philip Lundrigan <philiplundrigan@gmail.com>
2018-02-07 15:05:59 -0700

    Add frame for two 1's and refine callbacks

--- src/main.c
+++ src/main.c
@@ -19,59 +19,112 @@
 #include "mgos_timers.h"
 
 
-uint8_t packet[128] = { 0x80, 0x00, 0x00, 0x00,
-                /*4*/   0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-                /*10*/  0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
-                /*16*/  0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
-                /*22*/  0xc0, 0x6c,
-                /*24*/  0x83, 0x51, 0xf7, 0x8f, 0x0f, 0x00, 0x00, 0x00,
-                /*32*/  0x64, 0x00,
-                /*34*/  0x01, 0x04,
-                /* SSID */
-                /*36*/  0x00, 0x06, 0x72, 0x72, 0x72, 0x72, 0x72, 0x72,
-                        0x01, 0x08, 0x82, 0x84,
-                        0x8b, 0x96, 0x24, 0x30, 0x48, 0x6c, 0x03, 0x01,
-                /*56*/  0x0b};
+uint8_t one_packet[57] = {0x80, 0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0xff,
+                          0xff, 0xff, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
+                          0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0xc0, 0x6c,
+                          0x83, 0x51, 0xf7, 0x8f, 0x0f, 0x00, 0x00, 0x00,
+                          0x64, 0x00, 0x01, 0x04, 0x00, 0x06, 0x72, 0x72,
+                          0x72, 0x72, 0x72, 0x72, 0x01, 0x08, 0x82, 0x84,
+                          0x8b, 0x96, 0x24, 0x30, 0x48, 0x6c, 0x03, 0x01,
+                          0x0b};
 
+uint8_t two_packet[114] = {0x80, 0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0xff,
+                           0xff, 0xff, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
+                           0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0xc0, 0x6c,
+                           0x83, 0x51, 0xf7, 0x8f, 0x0f, 0x00, 0x00, 0x00,
+                           0x64, 0x00, 0x01, 0x04, 0x00, 0x06, 0x72, 0x72,
+                           0x72, 0x72, 0x72, 0x72, 0x01, 0x08, 0x82, 0x84,
+                           0x8b, 0x96, 0x24, 0x30, 0x48, 0x6c, 0x03, 0x01,
+                           0x0b, 0x80, 0x00, 0x00, 0x00, 0xff, 0xff, 0xff,
+                           0xff, 0xff, 0xff, 0x01, 0x02, 0x03, 0x04, 0x05,
+                           0x06, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0xc0,
+                           0x6c, 0x83, 0x51, 0xf7, 0x8f, 0x0f, 0x00, 0x00,
+                           0x00, 0x64, 0x00, 0x01, 0x04, 0x00, 0x06, 0x72,
+                           0x72, 0x72, 0x72, 0x72, 0x72, 0x01, 0x08, 0x82,
+                           0x84, 0x8b, 0x96, 0x24, 0x30, 0x48, 0x6c, 0x03,
+                           0x01, 0x0b};
 
-uint8_t symbol[64] = {1, 1, 1, 1, 1, 1, 0, 1,
+uint8_t symbol[63] = {1, 1, 0, 1, 0, 1, 0, 1,
                       0, 1, 0, 1, 1, 0, 0, 1,
-                      1, 0, 1, 1, 1, 0, 1, 1,
-                      0, 1, 0, 0, 1, 0, 0, 1,
+                      1, 0, 1, 1, 0, 0, 1, 1,
+                      0, 1, 0, 0, 1, 0, 0, 0,
                       1, 1, 0, 0, 0, 1, 0, 1,
-                      1, 1, 1, 0, 0, 1, 0, 1,
+                      1, 0, 1, 0, 0, 1, 0, 1,
                       0, 0, 0, 1, 1, 0, 0, 0,
-                      0, 1, 0, 0, 0, 0, 0};
+                      0, 1, 0, 0, 0, 0, 1};
 
 uint8_t s_index = 0;
 
-
-static void inject_timer_cb(void *arg) {
-    int beacon_size = (int)arg;
-
-    if(s_index >= 64) {
+static void send_symbol() {
+    if(s_index >= 63) {
         // Nothing left to do
         return;
     }
 
-    if(symbol[s_index] == 1) {
-        wifi_send_pkt_freedom(packet, beacon_size, 0);
-    }
+    // Check for two 1 in a row
+    if(s_index < 61 && symbol[s_index] == 1 && symbol[s_index + 1] == 1) {
+        // printf("Sending two packets: %u\n", s_index);
+        int result = wifi_send_pkt_freedom(two_packet, 114, 0);
 
-    s_index += 1;
+        if(result == -1) {
+            printf("Unable to send two packet: %u\n", s_index);
+        }
+
+        // Increment by 2 because we sent two frames
+        s_index += 2;
+    }
+    // Check for one 1
+    else if(symbol[s_index] == 1) {
+        // printf("Sending one packet: %u\n", s_index);
+        int result = wifi_send_pkt_freedom(one_packet, 57, 0);
+
+        if(result == -1) {
+            printf("Unable to send one packet: %u\n", s_index);
+        }
+
+        // Just sent one frame
+        s_index += 1;
+    }
+    // Just a 0
+    else {
+        // printf("Sleeping: %u\n", s_index);
+        mgos_usleep(100);
+
+        // Go to the next chip in the symbol
+        s_index += 1;
+
+        send_symbol();
+    }
+}
+
+
+static void send_pkt_freedom_cb(uint8 status) {
+    send_symbol();
+    (void) status;
+}
+
+static void start(void *arg) {
+    printf("Starting...\n");
+    send_symbol();
+
+    (void) arg;
 }
 
 enum mgos_app_init_result mgos_app_init(void) {
     int pause_time = mgos_sys_config_get_onpc_pause_time();
     int beacon_size = mgos_sys_config_get_onpc_beacon_size();
-    printf("Entering inject mode!\n");
-    printf("Pause time: %d\n", pause_time);
-    printf("Beacon size: %d\n", beacon_size);
+
     wifi_set_opmode(STATION_MODE);
     wifi_set_channel(11);
 
-    mgos_set_hw_timer(pause_time, MGOS_TIMER_REPEAT, inject_timer_cb, (void *)beacon_size);
-    printf("Done inject mode!\n");
+    wifi_register_send_pkt_freedom_cb(send_pkt_freedom_cb);
+
+    printf("Entering inject mode!\n");
+    printf("Pause time: %d\n", pause_time);
+    printf("Beacon size: %d\n", beacon_size);
+
+    mgos_set_timer(3000, 0, start, NULL);
+
 
     return MGOS_APP_INIT_SUCCESS;
 }
