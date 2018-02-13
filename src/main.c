32b12010d0e39cb65d2673098c341dd16558212b
Philip Lundrigan <philiplundrigan@gmail.com>
2018-02-13 11:31:16 -0700

    Use hardware timer to test transmission period

--- src/main.c
+++ src/main.c
@@ -54,68 +54,67 @@ uint8_t symbol[63] = {1, 1, 0, 1, 0, 1, 0, 1,
                       0, 1, 0, 0, 0, 0, 1};
 
 uint8_t s_index = 0;
-
 unsigned int pause_time = 700;
-unsigned long start_time = 0;
-unsigned long end_time = 0;
+bool send = true;
 
-static void send_symbol() {
-    if(s_index >= 63) {
-        // Nothing left to do
-        return;
-    }
 
-    // Check for two 1 in a row
-    if(s_index < 61 && symbol[s_index] == 1 && symbol[s_index + 1] == 1) {
-        start_time = system_get_time();
-        // printf("Sending two packets: %u\n", s_index);
-        int result = wifi_send_pkt_freedom(two_packet, 114, 0);
+// static void send_symbol(void *arg) {
+//     if(s_index >= 63) {
+//         // Nothing left to do
+//         return;
+//     }
 
-        if(result == -1) {
-            printf("Unable to send two packet: %u\n", s_index);
-        }
+//     // Check for two 1 in a row
+//     if(s_index < 61 && symbol[s_index] == 1 && symbol[s_index + 1] == 1) {
+//         // printf("Sending two packets: %u\n", s_index);
+//         int result = wifi_send_pkt_freedom(two_packet, 114, 0);
 
-        // Increment by 2 because we sent two frames
-        s_index += 2;
-        end_time = system_get_time();
-    }
-    // Check for one 1
-    else if(symbol[s_index] == 1) {
-        start_time = system_get_time();
-        // printf("Sending one packet: %u\n", s_index);
+//         if(result == -1) {
+//             printf("Unable to send two packet: %u\n", s_index);
+//         }
+
+//         // Increment by 2 because we sent two frames
+//         s_index += 2;
+//     }
+//     // Check for one 1
+//     else if(symbol[s_index] == 1) {
+//         // printf("Sending one packet: %u\n", s_index);
+//         int result = wifi_send_pkt_freedom(one_packet, 57, 0);
+
+//         if(result == -1) {
+//             printf("Unable to send one packet: %u\n", s_index);
+//         }
+
+//         // Just sent one frame
+//         s_index += 1;
+//     }
+//     else {
+//         // Zero -- do nothing
+//         s_index += 1;
+//     }
+
+//     (void) arg;
+// }
+
+static void send_on_off(void *arg) {
+    if(send) {
         int result = wifi_send_pkt_freedom(one_packet, 57, 0);
 
-        if(result == -1) {
-            printf("Unable to send one packet: %u\n", s_index);
-        }
+        // if(result == -1) {
+        //     printf("Unable to send one packet: %u\n", s_index);
+        // }
 
-        // Just sent one frame
-        s_index += 1;
-        end_time = system_get_time();
+        (void) result;
     }
-    // Just a 0
-    else {
-        // Time between the last end and this start
-        unsigned long diff = pause_time - (end_time - start_time);
-        // printf("Sleeping: %u\n", s_index);
-        mgos_usleep(diff);
 
-        // Go to the next chip in the symbol
-        s_index += 1;
-
-        send_symbol();
-    }
+    send = !send;
+    (void) arg;
 }
 
-
-static void send_pkt_freedom_cb(uint8 status) {
-    send_symbol();
-    (void) status;
-}
 
 static void start(void *arg) {
     printf("Starting...\n");
-    send_symbol();
+    mgos_set_hw_timer(pause_time, MGOS_TIMER_REPEAT, send_on_off, NULL);
 
     (void) arg;
 }
@@ -126,8 +125,6 @@ enum mgos_app_init_result mgos_app_init(void) {
 
     wifi_set_opmode(STATION_MODE);
     wifi_set_channel(11);
-
-    wifi_register_send_pkt_freedom_cb(send_pkt_freedom_cb);
 
     printf("Entering inject mode!\n");
     printf("Pause time: %d\n", pause_time);
